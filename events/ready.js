00a249296c932de8b330e51f414cd90d5caf0ff6
NotTCA <realtcatech@gmail.com>
2022-05-05 17:54:43 +0800

    updated the list of statuses

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,11 @@ const client = require("../index");
 client.on("ready", () => {
   setInterval(() => {
     const list = [
-      ">help",
+      `${client.config.prefix}help`,
       `${client.users.cache.size} users`,
       `${client.guilds.cache.size} servers`,
       `${client.channels.cache.size} channels`,
+      "global.tcatech.ml",
       "the TCA Tech team",
     ];
     const randomStatus = list[Math.floor(Math.random() * list.length)];
